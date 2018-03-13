4a8eee399f3f1e659ce18753664d077c21e79e34
CHARFOUNI <abdelkarim.charfouni@capgemini.com>
2018-03-13 15:38:08 +0000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos',(req,res)=>{
     });
 });
 
+app.get('/todos',function(req,res){
+        Todo.find().then((todos)=>{
+            res.send({todos});
+        },(err)=>{
+            res.status(400).send(err);
+        });
+});
+
 app.listen(3000,()=>{
     console.log('server is up on port 3000');
 });
